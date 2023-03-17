a57ddf663d7dfaf53cacb89b978372cf7d9f9aea
Samuel <samnmbuguah@gmail.com>
2023-03-17 16:17:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function razzle() {
+    console.log("You've been razzled!");
+  }
+razzle()
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+ 
+const mondayWork = function(work = 'go to the office'){
+    return (`This Monday, I will ${work}.`);
+}
+
+
+function wrapAdjective(flair = "*"){
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+console.log(wrapAdjective("%")("a dedicated programmer"));
